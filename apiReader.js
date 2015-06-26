da3234c8f0b3891dae31310d5e7be54810e2f8b5
DutchProgrammer <danny@dutchprogrammer.nl>
2015-06-26 20:41:59 +0200

    npm and bower compatible

---
+++ apiReader.js
@@ -0,0 +1,48 @@
+var apiReader = new function() {
+
+	var settings = {};
+
+	function doRequest(url, method, data, contentType, headers) {
+		settings = {
+		  url: url,
+		  data: data,
+		  async: false,
+		  method: method,
+		  cache: false
+		};
+
+		if (headers !=== undefined && headers) {
+			settings['headers'] = headers;
+		}
+		if (data !=== undefined && data) {
+			settings['data'] = data;
+		}
+		if (contentType !=== undefined && contentType) {
+			settings['contentType'] = contentType;
+		}
+
+		return $.ajax().done(function() {
+		  $( this ).addClass( "done" );
+		});
+	};
+
+	this.setSettings = function setSettings(customSettings) {
+		settings = $.jquery(settings, customSettings);
+
+		return true;
+	};
+
+	this.get = function get(url, data, contentType, headers) {
+		return doRequest(url, 'GET', data, contentType, headers);
+	};
+
+	this.post = function post(url, data, contentType, headers) {
+		return doRequest(url, 'POST', data, contentType, headers);
+	};
+
+	this.put = function put(url, data, contentType, headers) {
+		return doRequest(url, 'PUT', data, contentType, headers);
+	};
+
+});
+
